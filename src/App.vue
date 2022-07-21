7355abf84a87386475d3f1e9728e4bf763b4f278
Analyce Ferreira <anallyce_@live.com>
2022-07-21 17:30:41 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import { ref } from 'vue'
+  import header from './components/header.vue'
+
+
+</script>
+
+
+<template>
+  <header/>
+  <div class="videoWrapper">
+    <iframe src="https://grupoicf.com.br/widgets/experts" allowfullscreen="" class=""></iframe>
+  </div>
+</template>
+
+
+<style scoped>
+
+.videoWrapper {
+    position: relative;
+    padding-bottom: 56.25%;
+    padding-top: 25px;
+    height: 0;
+    overflow: hidden;
+}
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,30 @@
+<script>
+export default({
+    name: "header"
+})
+</script>
+
+
+<template>
+    <header>
+    <div>
+        <img src="https://icfconstrutora.com.br/wp-content/uploads/2020/10/LADO-VERDE-FUNDO-BRANCO.png" alt="Logo ICF reduzida cor verde fundo">
+    </div>
+    <nav>
+        <a href="">Início</a>
+        <a href="">Licenciados</a>
+        <a href="">Experts</a>
+        <a href="">Obras</a>
+        <a href="">Calculadora</a>
+        <a href="">Orçamento</a>
+        <a href="">Trabalhe Conosco</a>
+    </nav>
+    </header>
+</template>
+
+<style scoped>
+    header {
+        background-color: black;
+        height: 100px;
+    }
+</style>
