d8aacd1dff57dd213203f06971a2cc450d1552ea
Analyce Ferreira <anallyce_@live.com>
2022-08-18 17:51:46 -0400

    BUILD: running build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/landing-page-icf-contrutora/',
   plugins: [vue()],
   resolve: {
     alias: {
